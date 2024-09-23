bec4356142d8c931a8e7a4481b11e3fadc5d8c6a
kaimyloveqwxz <142720065+kaimyloveqwxz@users.noreply.github.com>
2024-09-23 16:57:03 +0800

    :3333

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+    titleWeb: Secrettt
+    introTitle: you might be wondering what this is pero malalaman mo rin HAHAHAHAHA',
+    introDesc: sooooo, i just you to know na very thankful ako kasi nakilala kita, you're a very kind person, caring, loving, etc but
+   
+    
+    btnIntro: loveyou pogi🫶🏻
+    title: basta hulaan mo
+    desc: my thank you can never show how much i appreciate you, i wasn't expecting na makilala ka, and now here u are the person who shows me how easy i am to love:)
+    btnNo: pakiss nga HAHAHAHAHAH
+    question: do you know what this is?
+    btnReply: yes or no
+    reply: if no well malalaman mo now HAHAHAHAH,
+    mess: you're courting me at the moment diba?well you need to stop courting me I'm sorry ik we haven't been talking for that long and I'm already asking you to stop courting me, bc sinasagot na kita.
+    messDesc: I'M ALLOWING YOU TO BE MY BOYFRIEND NAPO:33
+    btnAccept: ILOVEYOUUUUUUUU<3
+    messLink:secrettttt
